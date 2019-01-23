eff4657bd8f10745f153dbd535ed0e449a42d243
manavm1990 <manavm@visionwebsoft.com>
2019-01-22 21:38:09 -0600

    Initial commit for Meetup pres.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-
+// Fetch most popular movies
+const movieResults = fetch(
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
+);
+console.log(movieResults);
