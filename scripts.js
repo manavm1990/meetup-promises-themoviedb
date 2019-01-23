48841900082bd8018b617631f04d733d806fbfd8
manavm1990 <manavm@visionwebsoft.com>
2019-01-22 22:57:16 -0600

    Show error handling.
    We have unhandled errors!

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 // Fetch most popular movies
 const movieResults = fetch(
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
+  "https://api.themoviejjdb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
 );
 movieResults
-  .then(response => response.json())
+  .then(
+    // ONLY 1 CAN EVER BE CALLED!
+    response => response.json(),
+    error => console.error(`Error! ${error}`)
+  )
 
   // PROMISE CHAIN 'prettier' than 'nesting callbacks!'
   .then(movies => movies.results.forEach(movie => console.log(movie.title)));
