8282d2a792981ec490453b46a0ad2cea0aaff258
manavm1990 <manavm@visionwebsoft.com>
2019-01-22 22:42:46 -0600

    Use PROMISE CHAIN.

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@
 const movieResults = fetch(
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
 );
-movieResults.then(response => {
-  response
-    .json()
-    .then(movies => movies.results.forEach(movie => console.log(movie.title)));
-});
+movieResults
+  .then(response => response.json())
+
+  // PROMISE CHAIN 'prettier' than 'nesting callbacks!'
+  .then(movies => movies.results.forEach(movie => console.log(movie.title)));
