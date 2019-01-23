fbeac6929672594608fdda41f99a261ff5964c59
manavm1990 <manavm@visionwebsoft.com>
2019-01-22 23:27:46 -0600

    Handle error with Promise.reject()

--- scripts.js
+++ scripts.js
@@ -1,21 +1,16 @@
 // Fetch most popular movies
 const movieResults = fetch(
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555xxx"
 );
 movieResults
-  .then(response =>
-    // TODO: Handle 'bad' responses like '404 by 'throwing' 'new Error'
-    response.json()
-  )
+  .then(response => {
+    if (!response.ok) {
+      return Promise.reject(new Error("bad response!"));
+    }
+    return response.json();
+  })
 
   // PROMISE CHAIN 'prettier' than 'nesting callbacks!'
   .then(movies => movies.results.forEach(movie => console.log(movie.title)))
-
-  /**
-   * ES8 introduced finally()
-   * It will execute no matter what!
-   * It can be placed anywhere, but usually at end
-   */
-  .finally(() => console.log("finally!"))
   .catch(error => console.error(`Error! ${error}`))
   .finally(() => console.log("all done!"));
