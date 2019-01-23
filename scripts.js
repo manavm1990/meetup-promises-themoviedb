5ba7c4b2960a4546866a1e2031dc594ec3b608d9
manavm1990 <manavm@visionwebsoft.com>
2019-01-22 22:36:22 -0600

    Log results.json()

--- scripts.js
+++ scripts.js
@@ -3,5 +3,5 @@ const movieResults = fetch(
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
 );
 movieResults.then(response => {
-  console.log(response.json());
+  response.json().then(movies => console.log(movies));
 });
