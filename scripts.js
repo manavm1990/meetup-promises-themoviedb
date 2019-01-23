9e7aa4bcbf732f8262655030304651313aa9a73e
manavm1990 <manavm@visionwebsoft.com>
2019-01-23 09:51:29 -0600

    Log Promise.all() to screen.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
-// Fetch most popular movies
-const movieResults = fetch(
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555xxx"
-);
-movieResults
-  .then(response => {
-    if (!response.ok) {
-      return Promise.reject(new Error("bad response!"));
-    }
-    return response.json();
-  })
+/**
+ * Fetch most popular movies AND movies currently in theatres AND most popular movies in 2009.
+ * Do so IN PARALLEL.
+ */
+const movieResults = Promise.all([
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
+  ),
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-22&primary_release_date.lte=2018-12-22&api_key=6891357a0c99af8e63ba31ebfc80a555"
+  ),
+  fetch(
+    "https://api.themoviedb.org/3/discover/movie?primary_release_year=2009&sort_by=vote_average.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
+  )
+]);
 
-  // PROMISE CHAIN 'prettier' than 'nesting callbacks!'
-  .then(movies => movies.results.forEach(movie => console.log(movie.title)))
-  .catch(error => console.error(`Error! ${error}`))
-  .finally(() => console.log("all done!"));
+console.log(movieResults);
