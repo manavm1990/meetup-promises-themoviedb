d77360fb47ec2a251a01f89c903c4b0ad3a6903a
manavm1990 <manavm@visionwebsoft.com>
2019-01-22 22:27:32 -0600

    Log 'raw' Promise response

--- scripts.js
+++ scripts.js
@@ -2,4 +2,6 @@
 const movieResults = fetch(
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
 );
-console.log(movieResults);
+movieResults.then(response => {
+  console.log(response);
+});
