0d17ee7d6f9e8b75f46a8c7abbfdc65f510ac134
manavm1990 <manavm@visionwebsoft.com>
2019-01-23 10:05:07 -0600

    See Promise.all() response value.
    It is an iterable with the results in the order of each of the Promises passed into the Promise.all()

--- scripts.js
+++ scripts.js
@@ -5,13 +5,13 @@
 const movieResults = Promise.all([
   fetch(
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
-  ),
+  ).then(response => response.json()),
   fetch(
-    "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-22&primary_release_date.lte=2018-12-22&api_key=6891357a0c99af8e63ba31ebfc80a555"
-  ),
+    "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2018-12-22&primary_release_date.lte=2019-01-22&api_key=6891357a0c99af8e63ba31ebfc80a555"
+  ).then(response => response.json()),
   fetch(
     "https://api.themoviedb.org/3/discover/movie?primary_release_year=2009&sort_by=vote_average.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
-  )
+  ).then(response => response.json())
 ]);
 
-console.log(movieResults);
+movieResults.then(response => console.log(response));
