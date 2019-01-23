22e13132c54b482d6f5b006e02225ff3228aeb0f
manavm1990 <manavm@visionwebsoft.com>
2019-01-22 22:38:13 -0600

    List titles.

--- scripts.js
+++ scripts.js
@@ -3,5 +3,7 @@ const movieResults = fetch(
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6891357a0c99af8e63ba31ebfc80a555"
 );
 movieResults.then(response => {
-  response.json().then(movies => console.log(movies));
+  response
+    .json()
+    .then(movies => movies.results.forEach(movie => console.log(movie.title)));
 });
